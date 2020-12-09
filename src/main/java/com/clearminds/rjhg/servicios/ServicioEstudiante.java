e1f181da15a49d0c362f9017dd02b5ff36044d20
Richard Heredia <elgeorh@gmail.com>
2020-12-09 11:30:01 -0500

    Insertar estudiante

--- src/main/java/com/clearminds/rjhg/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/rjhg/servicios/ServicioEstudiante.java
@@ -1,5 +1,8 @@
 package com.clearminds.rjhg.servicios;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import com.clearminds.rjhg.dtos.Estudiante;
 import com.clearminds.rjhg.excepciones.BDDException;
 
@@ -7,6 +10,19 @@ public class ServicioEstudiante extends ServicioBase {
 	public void insertarEstudiante(Estudiante estudiante) throws BDDException {
 		abrirConexion();
 		System.out.println("Insertando estudiante: " + estudiante);
-		cerrarConexion();
+		Statement stmt =null;
+		try {
+			stmt = getConexion().createStatement();
+			
+			String sql = "insert into estudiantes(nombre,apellido) values('" + 
+			estudiante.getNombre() + "','" + estudiante.getApellido() + "')";
+			
+			System.out.println("Script: " + sql);
+			stmt.executeUpdate(sql);
+			cerrarConexion();
+		} catch(SQLException e) {
+			e.printStackTrace();
+			throw new BDDException("Error al insertar estudiante");
+		}
 	}
 }
